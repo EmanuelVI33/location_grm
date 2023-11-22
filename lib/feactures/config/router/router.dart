f10e4e17110691afd3987c836a9b3773bdd342b8
Emanuel <emanuelvaca39@gmail.com>
2023-11-22 16:59:53 -0400

    Add Home

--- lib/feactures/config/router/router.dart
+++ lib/feactures/config/router/router.dart
@@ -1,6 +1,7 @@
 import 'package:go_router/go_router.dart';
+import 'package:location_grm/feactures/mapa/presentation/pages/home/home_page.dart';
 import 'package:location_grm/feactures/mapa/presentation/pages/mapa/mapa_page.dart';
-import 'package:location_grm/feactures/mapa/presentation/pages/mapa/welcome.dart';
+import 'package:location_grm/feactures/mapa/presentation/pages/solicitud/solicitud_page.dart';
 
 class RouterApp {
   final router = GoRouter(
@@ -9,12 +10,16 @@ class RouterApp {
       GoRoute(
           path: '/',
           builder: (context, state) {
-            return const Welcome();
+            return const HomePage();
           },
           routes: [
             GoRoute(
               path: 'mapa',
               builder: (context, state) => const MapaPage(),
+            ),
+            GoRoute(
+              path: 'solicitud',
+              builder: (context, state) => const SolicitudPage(),
             )
           ]),
     ],
